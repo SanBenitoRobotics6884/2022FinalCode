e4eb65bd3fe4167a7898c7e2cacae6c33f24d96d
Macbook <sbhs6884@gmail.com>
2022-02-19 18:37:49 -0800

    InitialBasicStorageFunction

--- StorageFinal/src/main/java/frc/robot/Robot.java
+++ StorageFinal/src/main/java/frc/robot/Robot.java
@@ -4,14 +4,24 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
+  static final double uprMtrSpd = 0.7;
+  static final double lwrMtrSpd = 0.3;
+  Joystick m_joystick = new Joystick(0);
+  WPI_TalonSRX m_upperMotor = new WPI_TalonSRX(5);
+  WPI_TalonSRX m_lowerMotor = new WPI_TalonSRX(6);
   
  
   @Override
   public void robotInit() {
-   
+   m_lowerMotor.configFactoryDefault();
+   m_upperMotor.configFactoryDefault();
+
   }
 
   @Override
@@ -34,7 +44,19 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    if (m_joystick.getRawButton(2)) {
+      m_lowerMotor.set(lwrMtrSpd);
+    } else {
+      m_lowerMotor.set(0);
+    }
+    if (m_joystick.getTrigger()) {
+      m_upperMotor.set(uprMtrSpd);
+    } else {
+      m_upperMotor.set(0);
+    }
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
