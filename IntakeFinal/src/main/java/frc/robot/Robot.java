8b33e4dba3c9dbf939a3636a46b62bf527b71dc7
NathanielF3 <nathanazeved915@gmail.com>
2022-02-24 14:22:24 -0800

    Add velocity pid

--- IntakeFinal/src/main/java/frc/robot/Robot.java
+++ IntakeFinal/src/main/java/frc/robot/Robot.java
@@ -5,16 +5,32 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends TimedRobot {
-  CANSparkMax m_intakeMotor = new CANSparkMax(3, MotorType.kBrushless);
-  Joystick m_joystick = new Joystick(0);
+  private CANSparkMax m_intakeMotor = new CANSparkMax(5, MotorType.kBrushless);
+  private Joystick m_joystick = new Joystick(0);
+  private static final double kP = 0.0005;
+  private static final double kI = 0;
+  private static final double kD = 0.0000;
+  private static final double kF = 0.0004;
+  private static final double kMinOutput = -1;
+  private static final double kMaxOutput = 1;
+  private static final double kTargetRPM = 1000;
+  private static final double kMotorGearing = 3;
+  private static final double kOpenLoopSpeed = 0.5;
+  private static final double kMaxVoltage = 12;
+  private SparkMaxPIDController m_Pid;
+  private RelativeEncoder m_encoder;
+  private static final boolean isPid = true;
+
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -22,10 +38,22 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    m_intakeMotor.restoreFactoryDefaults();
+    m_encoder = m_intakeMotor.getEncoder();
+    m_encoder.setVelocityConversionFactor(1/kMotorGearing);
+    m_Pid = m_intakeMotor.getPIDController();
+    m_Pid.setP(kP);
+    m_Pid.setI(kI);
+    m_Pid.setD(kD);
+    m_Pid.setIZone(0);
+    m_Pid.setFF(kF);
+    m_Pid.setOutputRange(kMinOutput, kMaxOutput);
   }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    SmartDashboard.putNumber("RPM", m_encoder.getVelocity());
+  }
 
   @Override
   public void autonomousInit() {
@@ -43,10 +71,18 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    if (m_joystick.getRawButton(2)) {
-      m_intakeMotor.set(0.4);
+    if (isPid) {
+      if (m_joystick.getTrigger()) {
+        m_Pid.setReference(kTargetRPM, CANSparkMax.ControlType.kVelocity);
+      } else {
+        m_intakeMotor.setVoltage(0);
+      }
     } else {
-      m_intakeMotor.set(0);
+      if (m_joystick.getRawButton(2)) {
+        m_intakeMotor.setVoltage(kOpenLoopSpeed * kMaxVoltage);
+      } else {
+        m_intakeMotor.setVoltage(0);
+      }
     }
   }
 
