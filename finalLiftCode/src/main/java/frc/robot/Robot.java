f8bab4d0d7ee00e2a8c2d79f893984d279d86125
NathanielF3 <nathanazeved915@gmail.com>
2022-02-24 16:28:38 -0800

    start pid

--- finalLiftCode/src/main/java/frc/robot/Robot.java
+++ finalLiftCode/src/main/java/frc/robot/Robot.java
@@ -8,8 +8,12 @@ import java.sql.Date;
 import java.sql.Time;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -19,6 +23,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends TimedRobot {
+  private RelativeEncoder m_leftEncoder;
+  private RelativeEncoder m_rightEncoder;
+  private static final double kP = 0;
+  private static final double kI = 0;
+  private static final double kD = 0;
+  private static final double kF = 0;
+  private static final double kMinOutput = -1;
+  private static final double kMaxOutput = 1;
+  private static final boolean isPid = false;
+  private SparkMaxPIDController m_leftPid;
+  private SparkMaxPIDController m_rightPid;
   Joystick m_joystick = new Joystick(0);
 
   CANSparkMax m_leftLift = new CANSparkMax(7, MotorType.kBrushless);
@@ -36,8 +51,27 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_leftLift.restoreFactoryDefaults();
     m_rightLift.restoreFactoryDefaults();
+
     m_leftAcuator.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
     m_rightAcuator.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+
+    m_rightEncoder = m_rightLift.getEncoder();
+    m_rightPid = m_rightLift.getPIDController();
+    m_rightPid.setP(kP);
+    m_rightPid.setI(kI);
+    m_rightPid.setD(kD);
+    m_rightPid.setIZone(0);
+    m_rightPid.setFF(kF);
+    m_rightPid.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_leftEncoder = m_leftLift.getEncoder();
+    m_leftPid = m_leftLift.getPIDController();
+    m_leftPid.setP(kP);
+    m_leftPid.setI(kI);
+    m_leftPid.setD(kD);
+    m_leftPid.setIZone(0);
+    m_leftPid.setFF(kF);
+    m_leftPid.setOutputRange(kMinOutput, kMaxOutput);
 
     
   }
@@ -67,6 +101,41 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+    if (isPid) {
+
+      
+
+    } else {
+      openLoopLift();
+    }
+
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+
+  public void openLoopLift() {
     double lift = m_joystick.getY();
     if (Math.abs(lift) < 0.1) {
       lift = 0;
@@ -97,29 +166,6 @@ public class Robot extends TimedRobot {
 
     prevLiftSpeed = lift;
 
-  }
+}
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
 }
