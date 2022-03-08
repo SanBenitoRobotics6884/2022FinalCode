eddcb9418e8fdcc19f1410c34128ece53c843a10
Nick Carpenedo <nicholas.carpenedo@gmail.com>
2022-03-08 00:39:13 -0800

    Complete drive subsystem

--- FullCodeCommand/src/main/java/frc/robot/Constants.java
+++ FullCodeCommand/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import java.security.KeyPair;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +16,26 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final class Drive {
+        public final class TurnRatePID {
+            public static final double kP = 0.008;
+            public static final double kI = 0;
+            public static final double kD = 0.00005;
+            public static final double kF = 0.01;
+        }
+
+        public static final int kLeftFrontMotor = 1;
+        public static final int kRightFrontMotor = 3;
+        public static final int kLeftBackMotor = 2;
+        public static final int kRightBackMotor = 4;
+
+        public static final double kDriveGearing = 10.71;
+        public static final double kWheelCircumference = 6 * Math.PI;
+        public static final double kSlowspd = 0.4;
+        public static final double kFastSpd = 0.8;
+        public static final double kdrivedeadband = 0.1;
+        public static final double kMaxTurn = 360;
+
+    }
+}
