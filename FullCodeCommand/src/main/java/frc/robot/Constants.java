2fc2b953aa9bf34d5a81bcdff4ea14e436b6f001
Nick Carpenedo <nicholas.carpenedo@gmail.com>
2022-03-11 22:34:29 -0800

    Invert sign on lift encoder position soft limit

--- FullCodeCommand/src/main/java/frc/robot/Constants.java
+++ FullCodeCommand/src/main/java/frc/robot/Constants.java
@@ -69,7 +69,7 @@ public final class Constants {
         public static final double kRatchetDeploy = 0;
         public static final double kRatchetRetract = -0.5;
         public static final double kRatchetDelay = 3;
-        public static final double kMaxHeight = -57; //encoder counts
+        public static final double kMaxHeight = 57; //encoder counts
 
         public static final int kSmartMotionSlot = 0;
         public static final int kLeftLiftID = 8;
