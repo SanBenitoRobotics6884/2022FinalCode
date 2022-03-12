933b7e222f090ae3be5730cf3db85426be14511b
Nick Carpenedo <nicholas.carpenedo@gmail.com>
2022-03-12 00:30:01 -0800

    Add Drive Distane command

---
+++ FullCodeCommand/src/main/java/frc/robot/commands/DriveDistance.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.DriveSubsystem;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class DriveDistance extends CommandBase {
+
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveSubsystem m_drive;
+
+  double m_distX, m_distY, m_rot;
+
+  public DriveDistance(DriveSubsystem subsystem, double distX, double distY, double rot) {
+    m_distX = distX;
+    m_distY = distY;
+    m_rot = rot;
+
+    m_drive = subsystem;
+    addRequirements(m_drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_drive.setPositionTarget(m_distX, m_distY, m_rot);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.driveTowardTarget();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return m_drive.atTargetPosition();
+  }
+}
